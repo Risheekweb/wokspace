151c2fdd0638103db9f660a809f71b02b63fa32e
rishi <risheekraobillakanti@gmail.com>
2022-05-01 15:53:35 +0530

    forms

--- justwork/src/app/app.module.ts
+++ justwork/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsComponent } from './forms/forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ justwork/src/app/forms/forms.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControlName, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.css']
+})
+export class FormsComponent implements OnInit {
+  details:any
+  constructor() { }
+
+  onSubmit(val:any){
+console.log(val)
+  }
+
+  ngOnInit(): void {
+    this.details= new FormGroup({
+      title:new FormControl(""),
+      description: new FormControl(""),
+      gender: new FormControl(""),
+      qualification:new FormControl(""),
+      skills: new FormControl(""),
+      salary: new FormControl(0)
+
+
+    })
+  }
+
+}
